d93e9b7dcd62f09aefa29999a308b6fa5a1618b7
Lorena <loreninhazanon@gmail.com>
2024-06-12 15:08:37 -0300

    aulinha 12/06

--- AULA 3/js/script.js
+++ AULA 3/js/script.js
@@ -8,6 +8,6 @@ const isLight = document.documentElement.classList.contains("light");
 
 const imagem = isLight ? "./img/foto2.jpeg" : "./img/foto1.jpeg"
 document.querySelector("#perfil img").setAttribute("src", imagem);
-const alt = isLight ? "Imagem de uma menina no campo" : "Imagem de uma menina na festa"
+const alt = isLight ? "Imagem de uma menina de dia" : "Imagem de uma menina na noite"
 document.querySelector("#perfil img").setAttribute("alt", alt);
 }
